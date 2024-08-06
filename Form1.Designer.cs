94dc40290eb6e4d47c3d25c450d4ca8c20b1f686
Reno Mardiputra <reno.mardiputra@gmail.com>
2024-08-06 14:11:28 +0700

    update p0.1
    update view of virtual pet program

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,294 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.nama = new System.Windows.Forms.Label();
+            this.usia = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.respon = new System.Windows.Forms.Label();
+            this.makan = new System.Windows.Forms.Button();
+            this.bermain = new System.Windows.Forms.Button();
+            this.progressMood = new System.Windows.Forms.ProgressBar();
+            this.progressStamina = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(55, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 32);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Nama";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(55, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 32);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Usia";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(55, 255);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 32);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Status";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(52, 355);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 32);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Respon";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(52, 455);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 32);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Mood";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(52, 555);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(119, 32);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Stamina";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(278, 55);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(22, 32);
+            this.label10.TabIndex = 6;
+            this.label10.Text = ":";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(278, 155);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(22, 32);
+            this.label11.TabIndex = 7;
+            this.label11.Text = ":";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(278, 255);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(22, 32);
+            this.label12.TabIndex = 8;
+            this.label12.Text = ":";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(278, 355);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(22, 32);
+            this.label13.TabIndex = 9;
+            this.label13.Text = ":";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(278, 455);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(22, 32);
+            this.label14.TabIndex = 10;
+            this.label14.Text = ":";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(278, 555);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(22, 32);
+            this.label15.TabIndex = 11;
+            this.label15.Text = ":";
+            // 
+            // nama
+            // 
+            this.nama.AutoSize = true;
+            this.nama.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nama.Location = new System.Drawing.Point(346, 55);
+            this.nama.Name = "nama";
+            this.nama.Size = new System.Drawing.Size(89, 32);
+            this.nama.TabIndex = 12;
+            this.nama.Text = "Nama";
+            // 
+            // usia
+            // 
+            this.usia.AutoSize = true;
+            this.usia.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usia.Location = new System.Drawing.Point(346, 155);
+            this.usia.Name = "usia";
+            this.usia.Size = new System.Drawing.Size(71, 32);
+            this.usia.TabIndex = 13;
+            this.usia.Text = "Usia";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.Location = new System.Drawing.Point(346, 255);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(95, 32);
+            this.status.TabIndex = 14;
+            this.status.Text = "Status";
+            // 
+            // respon
+            // 
+            this.respon.AutoSize = true;
+            this.respon.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.respon.Location = new System.Drawing.Point(346, 355);
+            this.respon.Name = "respon";
+            this.respon.Size = new System.Drawing.Size(112, 32);
+            this.respon.TabIndex = 15;
+            this.respon.Text = "Respon";
+            // 
+            // makan
+            // 
+            this.makan.Location = new System.Drawing.Point(1016, 420);
+            this.makan.Name = "makan";
+            this.makan.Size = new System.Drawing.Size(175, 67);
+            this.makan.TabIndex = 16;
+            this.makan.Text = "Beri Makan";
+            this.makan.UseVisualStyleBackColor = true;
+            // 
+            // bermain
+            // 
+            this.bermain.Location = new System.Drawing.Point(1016, 520);
+            this.bermain.Name = "bermain";
+            this.bermain.Size = new System.Drawing.Size(175, 67);
+            this.bermain.TabIndex = 17;
+            this.bermain.Text = "Bermain";
+            this.bermain.UseVisualStyleBackColor = true;
+            // 
+            // progressMood
+            // 
+            this.progressMood.Location = new System.Drawing.Point(352, 455);
+            this.progressMood.Name = "progressMood";
+            this.progressMood.Size = new System.Drawing.Size(609, 32);
+            this.progressMood.TabIndex = 18;
+            // 
+            // progressStamina
+            // 
+            this.progressStamina.Location = new System.Drawing.Point(352, 555);
+            this.progressStamina.Name = "progressStamina";
+            this.progressStamina.Size = new System.Drawing.Size(609, 32);
+            this.progressStamina.TabIndex = 19;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(800, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 350);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
             // 
             // TheWindows
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressStamina);
+            this.Controls.Add(this.progressMood);
+            this.Controls.Add(this.bermain);
+            this.Controls.Add(this.makan);
+            this.Controls.Add(this.respon);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.usia);
+            this.Controls.Add(this.nama);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "TheWindows";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Virtual Pet";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label nama;
+        private System.Windows.Forms.Label usia;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Label respon;
+        private System.Windows.Forms.Button makan;
+        private System.Windows.Forms.Button bermain;
+        private System.Windows.Forms.ProgressBar progressMood;
+        private System.Windows.Forms.ProgressBar progressStamina;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,6 @@ namespace virtual_pet
         {
             InitializeComponent();
         }
+
     }
 }
